5f76c9722bd0643ff78903c8ec3bb096e9b6f9a0
Alexandre Rintaud <alexandre.rintaud@gmail.com>
2023-12-18 10:58:10 +0100

    Corrections Camille RT4 OK

--- RT5/scripts/fading.py
+++ RT5/scripts/fading.py
@@ -4,6 +4,15 @@ from pathlib import Path
 import SimpleITK as sitk
 import pandas as pd
 
+# canal rouge
+a = -6.2517E-13
+b = 6.5866E-08
+c = - 2.4741E-03
+d = 3.3246E+01
+
+def etalonnage_X23(x):
+    return a*x**3 + b*x**2 + c*x + d
+
 tiff_files = list(Path('/Volumes/LEXAR/fading/new_tiff/').rglob('*.tif'))
 time = [str(f).split('/')[5] for f in tiff_files][::3]
 initial_date = '2023/11/15 09:14:00'
@@ -16,24 +25,25 @@ for t in time:
     date.append(str(tiff_files[0]).split('_')[3])
     hour.append(str(tiff_files[0]).split('_')[4])
     for tiff in tiff_files:
-        img_arr0 = 2**16 - sitk.GetArrayFromImage(sitk.ReadImage(str(tiff_files[0])))[520:600, 400:475].mean()
-        img_arr1 = 2**16 - sitk.GetArrayFromImage(sitk.ReadImage(str(tiff_files[1])))[520:600, 400:475].mean()
-        img_arr2 = 2**16 - sitk.GetArrayFromImage(sitk.ReadImage(str(tiff_files[2])))[520:600, 400:475].mean()
+        img_arr0 = sitk.GetArrayFromImage(sitk.ReadImage(str(tiff_files[0])))[520:600, 400:475].mean()
+        img_arr1 = sitk.GetArrayFromImage(sitk.ReadImage(str(tiff_files[1])))[520:600, 400:475].mean()
+        img_arr2 = sitk.GetArrayFromImage(sitk.ReadImage(str(tiff_files[2])))[520:600, 400:475].mean()
     img_arr_mean = np.mean([img_arr0, img_arr1, img_arr2])
 
     mean.append(img_arr_mean)
 
 data = [date, hour, mean]
 df = pd.DataFrame.from_records(data).T
-df.columns = ['Date', 'Heure', 'Moyenne pixel']
+df.columns = ['Date', 'Heure', 'PV']
 date_hour = []
 for i in range(len(df)):
     date_hour.append(df.iloc[i, 0][:4] + '/' + df.iloc[i, 0][4:6] + '/' + df.iloc[i, 0][6:] + ' ' + df.iloc[i, 1][:2] + ':' + df.iloc[i, 1][2:4])
 
 df['Date et heure'] = date_hour
 df['Date et heure'] = pd.to_datetime(df['Date et heure'])
-df['Durée (h)'] = (df['Date et heure'] - pd.to_datetime(initial_date)) / (1E9 * 3600)
-df['Dose (Gy)'] = df['Moyenne pixel'] * 2 / df['Moyenne pixel'].to_list()[-1]
+df['Durée (h)'] = pd.to_numeric((df['Date et heure'] - pd.to_datetime(initial_date)) / (1E9 * 3600))
+df['Dose (Gy)'] = etalonnage_X23(df['PV'])
+df['Ecart dose (%)'] = (df['Dose (Gy)'] - 1.999) / 1.999 * 100
 
 plt.figure(figsize=(12, 7))
 plt.scatter(df['Durée (h)'], df['Dose (Gy)'], marker='x')
@@ -41,6 +51,6 @@ plt.xlabel('Durée (h)')
 plt.ylabel('Dose (Gy)')
 plt.title('Effet de fading')
 plt.grid(ls='--')
-plt.ylim(1.9, 2.025)
 plt.show()
+print(df['Durée (h)'])
 print(df)
--- RT5/scripts/rdt_films.py
+++ RT5/scripts/rdt_films.py
@@ -12,14 +12,35 @@ for i in tif_files:
     pixel_spacing = sitk.ReadImage(str(i)).GetSpacing()[0]
 
 mean_arr /= 3
-mean_arr = - mean_arr
 rdt = np.zeros([1, mean_arr.shape[0]])
 for i in range(mean_arr.shape[1]):
     rdt += mean_arr[:, i]
 
-rdt /= mean_arr.shape[0]
-rdt /= rdt.max() / 100
-plt.plot([f * pixel_spacing for f in range(rdt.shape[1])], rdt[0])
+rdt = rdt[0] / mean_arr.shape[1]
+
+# canal rouge
+a = -6.2517E-13
+b = 6.5866E-08
+c = - 2.4741E-03
+d = 3.3246E+01
+
+def etalonnage_X23(x):
+    return a*x**3 + b*x**2 + c*x + d
+
+numbre_pixel = [f for f in range(mean_arr.shape[0])]
+depth = [f * pixel_spacing for f in numbre_pixel]
+data = [depth, rdt]
+
+df = pd.DataFrame.from_records(data).T
+df.columns = ['Profondeur (mm)', 'PV']
+df['Dose (Gy)'] = etalonnage_X23(df['PV'])
+df['Dose normalisée (%)'] = df['Dose (Gy)'] / df['Dose (Gy)'].max() * 100
+plt.figure(figsize=(12, 7))
+plt.plot(df['Profondeur (mm)'], df['Dose normalisée (%)'])
+plt.grid(ls='--')
+plt.xlabel('Profondeur (mm)')
+plt.ylabel('Dose relative (%)')
+plt.title('Rendement en profondeur X23 film')
 plt.show()
 
 # tif_files = list(Path('/Volumes/LEXAR/Fiches_DQ/RT/RT5/rdm_X23/').glob('*h*.tif'))
